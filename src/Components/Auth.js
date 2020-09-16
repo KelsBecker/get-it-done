bde1640bba3fe8f56dc965c089dc98da22e65974
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-09-16 17:12:59 -0400

    add initial set-up for auth

---
+++ src/Components/Auth.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './Login'
+import SignUp from './SignUp'
+
+function Auth(){
+  return(
+    <>
+    <header>Auth</header>
+      <Login/>
+      <SignUp/>
+    </>
+  )
+}
+
+export default Auth;
