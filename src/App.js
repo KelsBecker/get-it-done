2cea7325443cbdb7d793ccfb46c66ba029080313
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-09-17 17:52:51 -0400

    add background image and auth components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import './App.css';
+import './Styling/styles.css';
 import GetItDone from './Components/GetItDone'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <GetItDone />
-      </header>
     </div>
   );
 }
